2558b9057d47827ebf6f0d73224323484b64bec6
panmanzee <kowpan2549z@gmail.com>
2026-03-05 18:08:39 +0700

    AfterMerge
    อย่าพังนะไอ้เหีเย

--- testforproject/Migrations/20260305110227_new-aftermerge.cs
+++ testforproject/Migrations/20260305110227_new-aftermerge.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace testforproject.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class newaftermerge : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
